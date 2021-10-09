5d610cc18afabb5e9c03874167a2b6fcbcb0d050
Alfie Fitch <alfiefitch0@gmail.com>
2021-10-09 17:59:00 +0100

    Project Pages

--- scripts.js
+++ scripts.js
@@ -6,15 +6,18 @@ var prevScrollpos = window.pageYOffset;
 
 let upordown = "down";
 
+let project = false;
 
 window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
+    if(project == false){
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-50px";
+        }
+        prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
 }
 
 
@@ -48,3 +51,55 @@ function load_spotify(){
     });
 }
 
+
+function return_home(){
+    project = false;
+    location.replace("/#portfolio");
+}
+
+
+function to_project(name){
+    location.replace("/projects.html?project=" + name);
+}
+
+
+function check_url_data(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const project_name = urlParams.get("project");
+    project = true;
+    if(project_name == "fill-the-gapz"){
+        fill_the_gapz();
+    }else if (project_name == "apploy"){
+        apploy();
+    }else if(project_name == "fitch-network"){
+        fitch_network();
+    }
+}
+
+function fill_the_gapz(){
+    document.getElementById("mockup").innerHTML = '<img class = "project-mockup" src = "imgs/fillthegapz-mockup.png">';
+    document.getElementById("project-title").innerText = "Fill The Gapz";
+    document.getElementById("dates").innerText = "March 2021 - Present";
+    document.getElementById("status").innerHTML = '<h1 class = "working">In Development</h1>'
+    document.getElementById("languages").innerHTML = '<h1>JavaScript</h1> <h1>HTML</h1>  <h1>Firebase</h1>  <h1>CSS</h1>'
+    document.getElementById("project-description").innerText = "An online, multiplayer and responsive Cards against Humanity clone with custom cards and images."
+}
+
+function apploy(){
+    document.getElementById("mockup").innerHTML = '<img class = "project-mockup" src = "imgs/apploy-mockup.png">';
+    document.getElementById("project-title").innerText = "Apploy";
+    document.getElementById("dates").innerText = "Oct 2021 - Present";
+    document.getElementById("status").innerHTML = '<h1 class = "working">In Development</h1>'
+    document.getElementById("languages").innerHTML = '<h1>JavaScript</h1> <h1>HTML</h1>  <h1>Firebase</h1>  <h1>SCSS</h1>'
+    document.getElementById("project-description").innerText = "An online database of freelance software engineers available to hire - A place to display your work and get jobs."
+}
+
+function fitch_network(){
+    document.getElementById("mockup").innerHTML = '<img class = "project-mockup" src = "imgs/FN-mockup.png">';
+    document.getElementById("project-title").innerText = "Fitch Network";
+    document.getElementById("dates").innerText = "Oct 2019 - Dec 2019";
+    document.getElementById("status").innerHTML = '<h1 class = "complete">Complete</h1>'
+    document.getElementById("languages").innerHTML = '<h1>JavaScript</h1> <h1>HTML</h1>  <h1>3rd Party</h1>  <h1>CSS</h1>'
+    document.getElementById("project-description").innerText = "Online portal to access the services provided such as Plex and Movie requests."
+}
